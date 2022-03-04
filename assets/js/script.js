fa6295d16defeded23e086851a1ee5c30bfd6065
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-04 17:55:34 +0530

    user credintials stored in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,12 @@
-/* Author: 
 
-*/
+$(document).ready(function(){
 
+    localStorage.setItem('username1','saurabh96');
+    localStorage.setItem('password1','123456');
+    let username = $('.username').val();
+        
 
+});
 
 
 
