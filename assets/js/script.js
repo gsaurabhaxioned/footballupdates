a110bc8f80a29acf9eabb4db37bd966e441149fb
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-04 19:27:06 +0530

    js code started for load more functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,11 @@
 
-    localStorage.setItem('username1','saurabh96');
-    localStorage.setItem('password1','123456');
-
-    let user1 = localStorage.getItem('username1'),
-    pass1 = localStorage.getItem('password1','123456');
-    function login(event){
-        event.preventDefault();
-        let username = $('.username').val(),
-        password = $('.password').val();
-        if(username === user1 && password === pass1){
-            localStorage.setItem('validuser',true);
-            window.open('homepage.html');
-        }else{
-            alert('invalid username or password');
-            window.open('index.html');
-        }
-    }
     
    $(document).ready(function(){
+        let valid = localStorage.getItem('validuser');
+        if(valid !== 'true'){
+            window.location.href='index.html';
+        }else{
 
-        let valid = localStorage.getItem('valid');
-        if(valid !== true){
-            window.open('index.html');
         }
 
         $('.logout').click(function(){
@@ -29,8 +13,33 @@
             window.open('index.html');
         });
 
+        $('.match-result-load-more').click(function(){
+            alert();
+            $('.match-result').append(
+                "<div>hdhd bhd hbdh</div>"
+            )
+        })
+
    });
 
+   function login(event){
+       event.preventDefault();
+    localStorage.setItem('username1','saurabh96');
+    localStorage.setItem('password1','123456');
+
+    let user1 = localStorage.getItem('username1'),
+    pass1 = localStorage.getItem('password1','123456');
+    event.preventDefault();
+    let username = $('.username').val(),
+    password = $('.password').val();
+    if(username === user1 && password === pass1){
+        localStorage.setItem('validuser','true');
+        window.open('homepage.html');
+    }else{
+        alert('invalid username or password');
+        window.open('index.html');
+    }
+}
 
 
 
