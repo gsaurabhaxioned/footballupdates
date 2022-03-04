b931ee7d9e7f17568a3ac5c8b4aebcbb7cc57897
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-04 18:19:48 +0530

    js code added for user login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,29 @@
 
-$(document).ready(function(){
+    localStorage.setItem('username1','saurabh96');
+    localStorage.setItem('password1','123456');
 
-    let user1 = localStorage.setItem('username1','saurabh96');
-    let pass1 = localStorage.setItem('password1','123456');
-
+    let user1 = localStorage.getItem('username1'),
+    pass1 = localStorage.getItem('password1','123456');
     function login(event){
         event.preventDefault();
         let username = $('.username').val(),
         password = $('.password').val();
         if(username === user1 && password === pass1){
             window.open('homepage.html');
+            localStorage.setItem('validuser',true);
         }else{
-            window.open('index.html');
             alert('invalid username or password');
+            window.open('index.html');
         }
     }
     
+    window.onload = function(){
+        let valid = localStorage.getItem('valid');
+        if(valid !== true){
+            window.open('index.html');
+        }
+    }
         
-
-});
 
 
 
