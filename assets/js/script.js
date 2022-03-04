3f00597d4f39c22273a49fc557c108e02ced6bae
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-04 18:01:34 +0530

    js code for user auth in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,21 @@
 
 $(document).ready(function(){
 
-    localStorage.setItem('username1','saurabh96');
-    localStorage.setItem('password1','123456');
-    let username = $('.username').val();
+    let user1 = localStorage.setItem('username1','saurabh96');
+    let pass1 = localStorage.setItem('password1','123456');
+
+    function login(event){
+        event.preventDefault();
+        let username = $('.username').val(),
+        password = $('.password').val();
+        if(username === user1 && password === pass1){
+            window.open('homepage.html');
+        }else{
+            window.open('index.html');
+            alert('invalid username or password');
+        }
+    }
+    
         
 
 });
