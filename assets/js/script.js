3e8d5f2776be7b9e72c7697c15f7ecc1fa01d616
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-05 15:37:01 +0530

    js code for showing list in progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,137 @@
-
-    
-   $(document).ready(function(){
-        let valid = localStorage.getItem('validuser'),
-        i=4;
-        if(!(window.location.href === "file:///C:/Users/soura/OneDrive/Desktop/ajaxtask/football%20updates/index.html")){
-        if(!valid){
-            window.location.href='index.html';
-        }else{
-
-        }
+$(document).ready(function () {
+    let valid = localStorage.getItem('validuser'),
+        i = 4;
+    if (!(window.location.href === "file:///C:/Users/soura/OneDrive/Desktop/ajaxtask/football%20updates/index.html")) {
+        if (!valid) {
+            window.location.href = 'index.html';
+        } else {}
     }
+    $('.logout').click(function () {
+        localStorage.clear();
+        window.open('index.html');
+    });
+    $('.clubs-dropper').click(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
+            type: "GET",
+            success: function (data) {
+                let result = JSON.parse(data);
+                for (key in result.clubs) {
+                    $('.clublistmenu').append(`
+                            <li class='club'>${result.clubs[key].name}</li>
+                            `);
+                }
+            }
+        })
+    });
+    $('.clublistmenu').on('click', 'li', function () {
+        let clubname = $(this).html();
+        $('.team-info').html("");
+        $('.team-performance-box').html("");
+        $.ajax({
+            url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
+            type: "GET",
+            success: function (data) {
+                let result = JSON.parse(data);
+                for (key in result.clubs) {
+                    if (clubname === result.clubs[key].name) {
+                        $('.team-info').append(
+                            `<span class="team-name">Name: ${result.clubs[key].name}</span>
+                                <span class="team-code">Code: ${result.clubs[key].code}</span>
+                                <span class="country-name">${result.clubs[key].country}</span>`
+                        );
+                    }
+                }
+            }
+        })
+        $.ajax({
+            url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
+            type: "GET",
+            success: function (data) {
+                let result = JSON.parse(data);
+                for (key in result.matches) {
+                    if (result.matches[key].team1 === clubname || result.matches[key].team2 === clubname) {
+                        $('.team-performance-box').append(
+                            `<div class="team-results-match-info">
+                                <span class="Round">Round: ${result.matches[key].round}</span>
+                                <span class="match-date">Date:  ${result.matches[key].date}</span>
+                                <span class="teams-involved">${result.matches[key].team1}: ${result.matches[key].team2}</span>
+                                <span class="scores">${result.matches[key].score.ft[0]}:${result.matches[key].score.ft[1]}</spaan>
+                            </div>`
+                        )
+                    }
+                }
+                $('.team-results').on('click', 'button', function () {
+                    i += 5;
+                    let team_result = document.querySelectorAll('.team-results-match-info'),
+                        team_result_length = team_result.length;
 
-        $('.logout').click(function(){
-            localStorage.clear();
-            window.open('index.html');
-        });
+                    for (j = 0; j <= i; j++) {
+                        team_result[j].style.display = "flex";
+                    }
+                    if (i === team_result_length - 1) {
+                        this.style.display = "none";
+                    }
+                })
+            }
+        })
+    })
+    $('.matchdays-dropper').click(function () {
+        $.ajax({
+            url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
+            type: "GET",
+            success: function (data) {
+                let result = JSON.parse(data);
 
-        $('.match-result-load-more').click(function(){
-            for(i=0;i<=4;i++){
+                for (key in result.matches) {
+                    console.log(result.matches[key].round);
+                    $('.matchdays').append(`<li class='match-day'>${result.matches[key].round}</li>`);
+                }
+            }
+        })
+    });
+    $('.matchdays').on('click', 'li', function () {
+        $('.match-result-box').html("");
+        let match = $(this).html();
+        $.ajax({
+            url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
+            type: "GET",
+            success: function (data) {
+                let result = JSON.parse(data);
+                for (key in result.matches) {
+                    if (result.matches[key].round === match) {
+                        $('.match-result-box').append(
+                            `<div class="match-result-details">
+                                <span class="match-result-Round">Round: ${result.matches[key].round}</span>
+                                <span class="match-result-date">Date:  ${result.matches[key].date}</span>
+                                <p class="match-result-teams">
+                                    <a href="clublis.html" title="Team" target="_blank">${result.matches[key].team1} </a>: <a href="clublist.html"
+                                        title="Team" target="_blank">${result.matches[key].team2} </a></p>
+                                <span class="scores">${result.matches[key].score.ft[0]}:${result.matches[key].score.ft[1]}</span>
+                            </div>`
+                        )
+                    }
+                }
+                $('.match-result-box').append(
+                    `<button class="match-result-load-more">Show More</button> `
+                )
+            }
+        })
+        $('.match-result-box').on('click', 'button', function () {
+            i += 5;
+            let match_result = document.querySelectorAll('.match-result-details'),
+                match_result_length = match_result.length;
+
+            for (j = 0; j <= i; j++) {
+                match_result[j].style.display = "flex";
+            }
+            if (i === match_result_length - 1) {
+                this.style.display = "none";
+            }
+        })
+    })
+    $('.match-result-load-more').click(function () {
+        for (i = 0; i <= 4; i++) {
             $('.match-result-box').append(
                 `<div class="match-result-details">
                 <span class="match-result-Round">Round: Matchday 1</span>
@@ -28,194 +142,36 @@
                 <spaan class="scores">4:1</spaan>
             </div>`
             )
-            }
-        })
-
-        $('.load-more').click(function(){
-            
-            for(i=0;i<=4;i++){
-                $('.team-performance-box').append(
-                    `
-                <div class="team-results-match-info">
+        }
+    })
+    $('.load-more').click(function () {
+        for (i = 0; i <= 4; i++) {
+            $('.team-performance-box').append(
+                `<div class="team-results-match-info">
                 <span class="Round">Round: Matchday 1</span>
                 <span class="match-date">Date: 2019-08-09</span>
                 <span class="teams-involved">Liverpool FC : Aston Villa FC</span>
                 <span class="scores">4:1</spaan>
             </div>`
-                )
-                 
-            }
-        })
+            )
+        }
+    })
+});
 
-        $('.clubs-dropper').click(function(){
-            $.ajax({
-                url:"https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
-                type:"GET",
-                success:function(data){
-                        let result = JSON.parse(data);
-                        for(key in result.clubs){
-                            $('.clublistmenu').append(`
-                            <li class='club'>${result.clubs[key].name}</li>
-                            `);
-                        }                    
-                }
-            })
-        });
-
-        $('.clublistmenu').on('click','li',function(){
-            let clubname = $(this).html();
-            $('.team-info').html("");
-            $('.team-performance-box').html("");
-
-            $.ajax({
-                url:"https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
-                type:"GET",
-                success:function(data){
-                    let result = JSON.parse(data);
-                    for(key in result.clubs){
-                    if(clubname === result.clubs[key].name){
-                         $('.team-info').append(
-                                `<span class="team-name">Name: ${result.clubs[key].name}</span>
-                                <span class="team-code">Code: ${result.clubs[key].code}</span>
-                                <span class="country-name">${result.clubs[key].country}</span>`  
-                            );
-                    }
-                }
-                }
-            })
-
-            $.ajax({
-                url:"https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
-                type:"GET",
-                success:function(data){
-                    let result = JSON.parse(data);                  
-                     for(key in result.matches){
-                         if(result.matches[key].team1 === clubname || result.matches[key].team2 === clubname){
-                           
-
-                
-                            $('.team-performance-box').append(
-                                `<div class="team-results-match-info">
-                                <span class="Round">Round: ${result.matches[key].round}</span>
-                                <span class="match-date">Date:  ${result.matches[key].date}</span>
-                                <span class="teams-involved">${result.matches[key].team1}: ${result.matches[key].team2}</span>
-                                <span class="scores">${result.matches[key].score.ft[0]}:${result.matches[key].score.ft[1]}</spaan>
-                            </div>`
-                            )
-                         }       
-                     }
-
-                     $('.team-results').on('click','button',function(){
-                        i+=5;
-                let team_result=document.querySelectorAll('.team-results-match-info'),
-                  team_result_length = team_result.length;
-                    
-                for(j=0;j<=i;j++){
-                    team_result[j].style.display = "flex";
-                }
-                if(i === team_result_length-1){
-                    this.style.display = "none";
-                }
-                    })
-                }           
-            })
-        })
-        $('.matchdays-dropper').click(function(){
-            $.ajax({
-                url:"https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
-                type:"GET",
-                success:function(data){
-                        let result = JSON.parse(data);
-                       
-                         for(key in result.matches){
-                            console.log(result.matches[key].round);
-                            $('.matchdays').append(`<li class='match-day'>${result.matches[key].round}</li>`);
-                         }
-
-                    }           
-            })
-        });
-
-        $('.matchdays').on('click','li',function(){
-            $('.match-result-box').html("");
-            let match = $(this).html();
-           
-            $.ajax({
-                url:"https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
-                type:"GET",
-                success:function(data){
-                        let result = JSON.parse(data);                   
-                         for(key in result.matches){
-                             if(result.matches[key].round === match){
-                            $('.match-result-box').append(
-                                `<div class="match-result-details">
-                                <span class="match-result-Round">Round: ${result.matches[key].round}</span>
-                                <span class="match-result-date">Date:  ${result.matches[key].date}</span>
-                                <p class="match-result-teams">
-                                    <a href="clublis.html" title="Team" target="_blank">${result.matches[key].team1} </a>: <a href="clublist.html"
-                                        title="Team" target="_blank">${result.matches[key].team2} </a></p>
-                                <span class="scores">${result.matches[key].score.ft[0]}:${result.matches[key].score.ft[1]}</span>
-                            </div>`
-                            )
-                             }
-                          
-                         }
-                         $('.match-result-box').append(
-                             `<button class="match-result-load-more">Show More</button> `
-                         )
-                    }           
-            })
-                $('.match-result-box').on('click','button',function(){
-                    i+=5;
-            let match_result=document.querySelectorAll('.match-result-details'),
-              match_result_length = match_result.length;
-                
-            for(j=0;j<=i;j++){
-                match_result[j].style.display = "flex";
-            }
-            if(i === match_result_length-1){
-                this.style.display = "none";
-            }
-                })
-          
-        })
-
-   });
-
-   function login(event){
-       event.preventDefault();
-    localStorage.setItem('username1','saurabh96');
-    localStorage.setItem('password1','123456');
-
+function login(event) {
+    event.preventDefault();
+    localStorage.setItem('username1', 'saurabh96');
+    localStorage.setItem('password1', '123456');
     let user1 = localStorage.getItem('username1'),
-    pass1 = localStorage.getItem('password1','123456');
+        pass1 = localStorage.getItem('password1', '123456');
     event.preventDefault();
     let username = $('.username').val(),
-    password = $('.password').val();
-    if(username === user1 && password === pass1){
-        localStorage.setItem('validuser','true');
+        password = $('.password').val();
+    if (username === user1 && password === pass1) {
+        localStorage.setItem('validuser', 'true');
         window.open('homepage.html');
-    }else{
+    } else {
         alert('invalid username or password');
         window.open('index.html');
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
