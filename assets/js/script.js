f9427651aea4cf6d89e346e8c3f3845c20ab1361
saurabh-gaonkar <saurabhg@axioned.com>
2022-03-15 14:37:08 +0530

    focus out effect added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,36 +1,41 @@
 $(document).ready(function () {
-//hamburger functionality
-        $(".hamburger").click(function () {
-            $(".headermenus").slideToggle();
-            $(this).toggleClass("cross");
-        });
- 
+    //hamburger functionality
+    $(".hamburger").click(function () {
+        $(".menus").toggleClass("headermenus-show");
+        $(".menus").toggleClass("headermenus");
+        $(this).toggleClass("cross");
+    });
+
     let valid = localStorage.getItem('validuser'),
         url = new URL(window.location.href),
         urlstring = url.search.slice(1),
         searchurlparam = new URLSearchParams(urlstring),
         paramvalue = searchurlparam.get('a'),
-        i=4;
-        //for checking if page redirected form match details page
+        i = 4;
+    //for checking if page redirected from match details page
     if (paramvalue === null) {
         console.log("null");
     } else {
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.clubs) {
                     $('.clublistmenu').append(`
                             <li class='club'>${result.clubs[key].name}</li>
                             `);
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.clubs) {
                     if (result.clubs[key].name === paramvalue) {
@@ -41,12 +46,16 @@ $(document).ready(function () {
                         );
                     }
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.matches) {
                     if (result.matches[key].team1 === paramvalue || result.matches[key].team2 === paramvalue) {
@@ -61,35 +70,48 @@ $(document).ready(function () {
                     }
                 }
                 changecolor(paramvalue);
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error)
             }
         })
     }
     // to check user login
-    if (!($(".login-page")[0])) {
+    if (!($(".login-page").length > 0)) {
         if (!valid) {
             window.location.href = 'index.html';
-        } else {}
+        }
+    } else {
+        if (valid) {
+            window.history.back();
+        }
     }
 
+
     //logout
-    $('.logout').click(function () {
+    $('.logout').click(() => {
         localStorage.clear();
         window.open('index.html');
     });
 
     //clubs dropdown
-    $('.clubs-dropper').click(function () {
+    $('.clubs-dropper').click(() => {
         $('.clublistmenu').html("");
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.clubs) {
                     $('.clublistmenu').append(`
                             <li class='club'>${result.clubs[key].name}</li>
                             `);
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
     });
@@ -106,7 +128,7 @@ $(document).ready(function () {
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.clubs) {
                     if (clubname === result.clubs[key].name) {
@@ -117,12 +139,16 @@ $(document).ready(function () {
                         );
                     }
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.matches) {
                     if (result.matches[key].team1 === clubname || result.matches[key].team2 === clubname) {
@@ -130,37 +156,42 @@ $(document).ready(function () {
                             `<div class="team-results-match-info">
                                 <span class="Round">Round: ${result.matches[key].round}</span>
                                 <span class="match-date">Date:  ${result.matches[key].date}</span>
-                                <a href="clublist.html?a=${result.matches[key].team1}" class="teams-involved">${result.matches[key].team1}</a>:<a href="clublist.html?a=${result.matches[key].team2}" class="teams-involved"> ${result.matches[key].team2}</a>
+                                <p>
+                                <a href="clublist.html?a=${result.matches[key].team1}" class="teams-involved">${result.matches[key].team1}</a>:<a href="clublist.html?a=${result.matches[key].team2}" class="teams-involved"> ${result.matches[key].team2}</a></p>
                                 <span class="scores">${result.matches[key].score.ft[0]}:${result.matches[key].score.ft[1]}</spaan>
                             </div>`
                         )
                     }
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
     })
 
     //load more on club list page
-    $('.team-results').on('click', 'button', function () {
+    $('.team-results').on('click', 'button', () => {
         i += 5;
 
-        let team_result = document.querySelectorAll('.team-results-match-info'),
+        let team_result = $('.team-results-match-info'),
             team_result_length = team_result.length - 1;
         for (j = 0; j <= i; j++) {
             team_result[j].style.display = "flex";
             if (j === team_result_length) {
-                document.querySelector('.load-more').style.display = "none";
+                $('.load-more').style.display = "none";
             }
         }
     })
 
     // match days dropper
-    $('.matchdays-dropper').click(function () {
+    $('.matchdays-dropper').click(() => {
         $('.matchdays').html("");
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data),
                     uniqueresult = [];
                 console.log(uniqueresult);
@@ -171,6 +202,10 @@ $(document).ready(function () {
                 for (key in getuniquevalue) {
                     $('.matchdays').append(`<li class='match-day'>${getuniquevalue[key]}</li>`);
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
     });
@@ -185,7 +220,7 @@ $(document).ready(function () {
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2019-20/en.1.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.matches) {
                     if (result.matches[key].round === match) {
@@ -204,16 +239,21 @@ $(document).ready(function () {
                 $('.match-result-box').append(
                     `<button class="match-result-load-more">Show More</button> `
                 )
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
     })
 
     //load more on match details
-    $('.match-result-box').on('click', 'button', function () {
+    $('.match-result-box').on('click', 'button', () => {
         i += 5;
-        let match_result = document.querySelectorAll('.match-result-details'),
+        let match_result = $('.match-result-details'),
             match_result_length = match_result.length - 1;
         for (j = 0; j <= i; j++) {
+            console.log(j);
             match_result[j].style.display = "flex";
             if (j === match_result_length) {
                 $('.match-result-load-more').css("display", "none");
@@ -222,57 +262,108 @@ $(document).ready(function () {
     })
 
     //redirect on clublist if team name cliked
-    $('.match-result-box').on('click', 'a', function () {
+    $('.match-result-box').on('click', 'a', () => {
         $.ajax({
             url: "https://raw.githubusercontent.com/openfootball/football.json/master/2015-16/en.1.clubs.json",
             type: "GET",
-            success: function (data) {
+            success: (data) => {
                 let result = JSON.parse(data);
                 for (key in result.clubs) {
                     $('.clublistmenu').append(`
                             <li class='club'>${result.clubs[key].name}</li>
                             `);
                 }
+            },
+            error: (error) => {
+                alert("something went wrong");
+                console.log(error);
             }
         })
     })
 });
-let loginform = document.querySelector('.loginform');
 
-    //login validation
-   loginform.addEventListener('submit',function(event) {
-    event.preventDefault();
-    let usererror = $('.usernameerror'),
+// const allInputs = $('form input');
+
+// const validateInputField = (input, errorField) => {
+//     console.log('input', input.attr('class'), errorField);
+//     if (input.attr('class') === 'username' && input.val() === '') {
+//         errorField.html('please enter firstname');
+//     } else if (input.attr('class') === 'password' && input.val() === '') {
+//         errorField.href('please enter lastname');
+//     } 
+
+// }
+
+// $.each(allInputs,(i)=>{
+//     i.blur(() => {
+//         if (input.type !== 'checkbox' && input.type !== 'submit') {
+//             validateInputField(input, input.nextSibling.nextSibling);
+//         }
+//     })
+// })
+    
+
+
+let loginform = $('.loginform'),
+    username = $('.username'),
+    password = $('.password'),
+    usererror = $('.usernameerror'),
     passerror = $('.passworderror');
-       usererror.html("");
-       passerror.html("");
-    
-    localStorage.setItem('username1', 'saurabh96');
-    localStorage.setItem('password1', '123456');
-    let user1 = localStorage.getItem('username1'),
-        pass1 = localStorage.getItem('password1', '123456');
-    event.preventDefault();
-    let username = $('.username').val(),
-        password = $('.password').val();
-    
+localStorage.setItem('username1', 'saurabh96');
+localStorage.setItem('password1', '123456');
+console.log(username);
 
-        if(username === ""){
-            usererror.html("please enter username");
-            $('.username').addClass("errorbox");
-        }else if(password === ""){
-            passerror.html("please enter password")
-            $('.password').addClass("errorbox");
-        }else if (username === user1 && password === pass1) {
+//blur validation
+username.focusout(() => {
+    usererror.html("");
+    passerror.html("");
+    $('.username').removeClass('errorbox');
+    $('.password').removeClass('errorbox');
+    if (username.val().length === 0) {
+        usererror.html("please enter username");
+        $('.username').addClass("errorbox");
+    }
+});
+
+password.focusout(() => {
+    usererror.html("");
+    passerror.html("");
+    $('.username').removeClass('errorbox');
+    $('.password').removeClass('errorbox');
+    if (password.val().length === 0) {
+        passerror.html("please enter password");
+        $('.password').addClass("errorbox");
+    }
+})
+//login validation
+loginform.submit((event) => {
+    $('.username').removeClass('errorbox');
+    $('.password').removeClass('errorbox');
+    event.preventDefault();
+
+    usererror.html("");
+    passerror.html("");
+    let user1 = localStorage.getItem('username1'),
+        pass1 = localStorage.getItem('password1');
+    event.preventDefault();
+
+    if (username.val().length === 0) {
+        usererror.html("please enter username");
+        $('.username').addClass("errorbox");
+    } else if (password.val().length === 0) {
+        passerror.html("please enter password")
+        $('.password').addClass("errorbox");
+    } else if (username.val() === user1 && password.val() === pass1) {
         localStorage.setItem('validuser', 'true');
         window.open('homepage.html');
     } else {
         passerror.html("invalid username or password");
     }
-}) 
+})
 
 // to change color of club name when clicked
-function changecolor(paramvalue) {
-    let clubnames = document.querySelectorAll('.clublist li');
+const changecolor = (paramvalue) => {
+    let clubnames = $('.clublist li');
     clubnames.forEach(i => {
         i.classList.remove("yellow");
         console.log(i);
